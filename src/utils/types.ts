f9be06846a7adff143a965b75cefab868512c6c9
sriail <mackinleymorrone@gmail.com>
2025-10-27 16:23:34 -0400

    Add Yahoo and Quant search engines to SearchEngines

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -12,9 +12,12 @@ type DropdownOptions = {
 };
 
 const SearchEngines: Record<string, string> = {
+    Bing: "https://bing.com/search?q=%s",
+    Brave: "https://search.brave.com/search?q=%s",
     DuckDuckGo: "https://duckduckgo.com/?q=%s",
     Google: "https://google.com/search?q=%s",
-    Bing: "https://bing.com/search?q=%s"
+    Yahoo: "https://search.yahoo.com/search?p=%s",
+    Quant: "https://www.qwant.com/?q=%s"
 };
 
 export { type SettingsProps, type DropdownOptions, SearchEngines };
